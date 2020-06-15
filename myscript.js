bf3b2e66eaf3146dbe556c2b002e53cd434dde59
luck07051 <43342441+luck07051@users.noreply.github.com>
2020-06-15 08:50:05 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,8 @@
 var zx, zy, inx, iny, textr, texte;
 var inTimes = 0,
     outTimes = 0;
+document.getElementById("R").innerHTML='';
+document.getElementById("X").innerHEML='';
 
 function main() {
     //input
